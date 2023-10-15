34905f239af9ae62216bcd3b355b30e0ebdacd97
kolliu <vania.martynyuck@yandex.ru>
2023-10-15 03:45:26 +0300

    fix admin panel

--- main/visits/urls.py
+++ main/visits/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 
 from visits.views import SalePointAPIView, VisitAPIView
 
-urlpatterns = [path("api/v1/sale_point_list", SalePointAPIView.as_view()),
-               path("api/v1/make_visit", VisitAPIView.as_view()),
+urlpatterns = [path("sale_point_list", SalePointAPIView.as_view()),
+               path("make_visit", VisitAPIView.as_view()),
                ]
